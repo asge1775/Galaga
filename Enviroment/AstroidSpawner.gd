6dd9eff050e14abf0d87a2a8b15817a0d9d27a42
Renen <36169704+asge1775@users.noreply.github.com>
2020-04-30 17:05:37 +0200

    Random Astroids

--- Enviroment/AstroidSpawner.gd
+++ Enviroment/AstroidSpawner.gd
@@ -3,12 +3,10 @@ extends Node2D
 var runs = 0
 
 var spawnTime
-var spawnTimeMin = 60
-var spawnTimeMax = 420
+var spawnTimeLim = [60,240]
 
-var spawnSpeedMin = 25
-var spawnSpeedMax = 200
-var spawnAngleMax = PI/2
+var spawnXSpeed = [25,100]
+var spawnYSpeed = [-50,50]
 
 var spawnWidth = OS.get_window_size().x
 var spawnHeight = 0
@@ -24,12 +22,12 @@ func spawn_astroid(position, direction):
 	astroid.velocity = direction
 
 func _ready():
-	spawnTime = rand_range(spawnTimeMin, spawnTimeMax)
+	spawnTime = rand_range(spawnTimeLim[0], spawnTimeLim[1])
 	
 
 func _process(delta):
 	if runs >= spawnTime:
-		spawnTime = rand_range(spawnTimeMin, spawnTimeMax)
+		spawnTime = rand_range(spawnTimeLim[0], spawnTimeLim[1])
 		runs = 0
-		spawn_astroid(Vector2(rand_range(0,spawnWidth),0), Vector2(0,rand_range(spawnSpeedMin,spawnSpeedMax)))
+		spawn_astroid(Vector2(rand_range(0,spawnWidth),0), Vector2(rand_range(spawnYSpeed[0],spawnYSpeed[1]),rand_range(spawnXSpeed[0],spawnXSpeed[1])))
 	runs += 1
