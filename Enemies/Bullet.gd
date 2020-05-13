0918783363b445b77fc59cf73de3166752f6ca37
Renen <36169704+asge1775@users.noreply.github.com>
2020-05-13 17:28:08 +0200

    shoy shootie

--- Enemies/Bullet.gd
+++ Enemies/Bullet.gd
@@ -3,14 +3,13 @@ extends KinematicBody2D
 export var speed = 500
 var velocity = Vector2.ZERO
 
-
 func _physics_process(delta):
-	velocity.y = speed * -1
+	velocity.y = speed * 1
 	move_and_slide(velocity)
 
-func _on_VisibilityNotifier2D_screen_exited():
+func _on_VisibilityNotifier2D_viewport_exited(viewport):
 	queue_free()
 
 
 func _on_Shootbox_area_entered(area):
-	queue_free()
+	pass # Replace with function body.
--- Enemies/Shoy.gd
+++ Enemies/Shoy.gd
@@ -1,8 +1,20 @@
 extends KinematicBody2D
+onready var timer = get_node("../../../SpawnTimer")
+
+var runs = 0
+export var shootInterval = 60
+export var shootStart = 0
 
 func _on_Hurtbox_area_entered(area):
 	create_explosion_effect()
 	queue_free()
+
+func shoot():
+	var world = get_tree().current_scene
+	var Bullet = load("res://Enemies/Bullet.tscn")
+	var bullet = Bullet.instance()
+	world.add_child(bullet)
+	bullet.global_position = Vector2(global_position.x, global_position.y + 10)
 
 func create_explosion_effect():
 	var world = get_tree().current_scene
@@ -11,3 +23,9 @@ func create_explosion_effect():
 	
 	world.add_child(explosionEffect)
 	explosionEffect.global_position = global_position
+
+func _process(delta):
+	if runs >= shootInterval and timer.wait_time - timer.time_left >= shootStart:
+		runs = 0
+		shoot()
+	runs += 1
